634a910bca2f0eb40c1d180cd8a68b61bb80f2d5
Joaquin Rego <clavsyofficial@gmail.com>
2023-04-11 18:11:33 -0300

    Ejercicios Clase 19, 20, 21, 22, 23 y 24 en Egg.

---
+++ EjerciciosExtra#1/src/ejerciciosextra/pkg1/EjerciciosExtra1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosextra.pkg1;
+
+import Services.RaicesService;
+
+/**
+ *
+ * @author clavs
+ */
+public class EjerciciosExtra1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        RaicesService ecuacion = new RaicesService();
+        ecuacion.llenarEcuacion();
+        ecuacion.calcular();
+        
+    }
+    
+}
